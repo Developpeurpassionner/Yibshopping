2d06eaa300bb62fc700f5a49041857e13929e7d7
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2025-08-17 00:40:16 +0200

    finalisation des designs des views formulaire_de_connexion_au_dasboard et du dashboard lui meme

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,30 +1,46 @@
 <template>
-  <div class="min-h-screen bg-gray-100 p-6">
-    <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Tableau de bord</h1>
+  <div class="min-h-screen bg-gray-100 p-6 flex flex-col gap-y-8 lg:gap-y-8 md:gap-y-16">
+
+    <h1 class="text-3xl font-bold mb-6 text-center text-gray-800 lg:text-3xl md:text-5xl">Tableau de bord</h1>
 
     <!-- Statistiques -->
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
-      <div class="bg-blue-100 p-6 rounded-lg shadow text-center">
-        <h2 class="text-xl font-semibold text-blue-800">Montres Hommes</h2>
-        <p class="text-4xl font-bold text-blue-900">{{ stats.hommes }}</p>
+      <div class="flex flex-col gap-y-4 lg:gap-y-4 md:gap-y-8">
+        <div class="bg-blue-100 p-6 rounded-lg shadow text-center">
+          <h2 class="text-xl font-semibold text-blue-800 lg:text-xl md:text-4xl">Montres Hommes</h2>
+          <p class="text-4xl font-bold text-blue-900 lg:text-4xl md:text-6xl">{{ stats.hommes }}</p>
+        </div>
+        <div class="flex justify-center">
+          <button class="cursor-pointer bg-blue-600 text-white text-xl lg:text-xl md:text-4xl px-4 py-1 rounded hover:bg-blue-500 transition">
+            Ajouter
+          </button>
+        </div>
       </div>
-      <div class="bg-pink-100 p-6 rounded-lg shadow text-center">
-        <h2 class="text-xl font-semibold text-pink-800">Montres Femmes</h2>
-        <p class="text-4xl font-bold text-pink-900">{{ stats.femmes }}</p>
+
+      <div class="flex flex-col gap-y-4 lg:gap-y-4 md:gap-y-8">
+        <div class="bg-pink-100 p-6 rounded-lg shadow text-center">
+          <h2 class="text-xl font-semibold text-pink-800 lg:text-xl md:text-4xl">Montres Femmes</h2>
+          <p class="text-4xl font-bold text-pink-900 lg:text-4xl md:text-6xl">{{ stats.femmes }}</p>
+        </div>
+        <div class="flex justify-center">
+          <button class="cursor-pointer bg-red-600 text-white text-xl lg:text-xl md:text-4xl px-4 py-1 rounded hover:bg-red-400 transition">
+            Ajouter
+          </button>
+        </div>
       </div>
     </div>
 
     <!-- Historique -->
     <div class="bg-white p-6 rounded-lg shadow overflow-x-auto">
-      <h2 class="text-2xl font-bold mb-4 text-gray-700">Historique des actions</h2>
+      <h2 class="font-bold text-center text-2xl lg:text-2xl md:text-4xl mb-4 text-gray-950">Historique des actions</h2>
       <table class="w-full text-left border-collapse">
         <thead>
           <tr class="bg-gray-200 text-gray-700">
-            <th class="p-2">Action</th>
-            <th class="p-2">Catégorie</th>
-            <th class="p-2">Nom</th>
-            <th class="p-2">Date</th>
-            <th class="p-2">Heure</th>
+            <th class="p-2 text-lg lg:text-base md:text-3xl">Nom</th>
+            <th class="p-2 text-lg lg:text-base md:text-3xl">Catégorie</th>
+            <th type="text" class="p-2 text-lg lg:text-base md:text-3xl">Description</th>
+            <th type="date" class="p-2 text-lg lg:text-base md:text-3xl">Date</th>
+            <th class="p-2 text-lg lg:text-base md:text-3xl">Heure</th>
           </tr>
         </thead>
         <tbody>
--- src/views/FormulaireDeConnexionAuPanneauAdmin.vue
+++ src/views/FormulaireDeConnexionAuPanneauAdmin.vue
@@ -1,36 +1,44 @@
 <template>
     <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-        <p>{{ message }}</p>
+        <p class="text-xl text-red-600 font-extrabold pb-6 lg:text-lg md:text-[35px]">{{ message }}</p>
         <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div
-                class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+            <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 
+                sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
             </div>
             <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
 
                 <div class="max-w-md mx-auto">
                     <div>
-                        <h1 class="text-2xl font-semibold">Login</h1>
+                        <h1 class="text-3xl font-semibold lg:text-2xl md:text-5xl">Login</h1>
                     </div>
                     <div class="divide-y divide-gray-200">
                         <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                             <div class="relative">
-                                <input autocomplete="off" id="Username" name="Username" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900
-                                     focus:outline-none focus:borer-rose-600" placeholder="Username"
-                                    v-model="AuthAdmin.username" />
-                                <label for="Username" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base
-                                     peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5
-                                      peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                                <input autocomplete="off" id="Username" name="Username" type="text" class="peer placeholder-transparent h-10 w-full border-b-2
+                                 border-gray-300 text-gray-900
+         text-xl sm:text-lg md:text-4xl lg:text-2xl
+         focus:outline-none" placeholder="Username" v-model="AuthAdmin.username" />
+                                <label for="Username" class="absolute left-0 -top-3.5 text-black
+         text-xl sm:text-base md:text-3xl lg:text-xl
+         transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-gray-400
+         peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
                             </div>
                             <div class="relative">
-                                <input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300
+                                <input autocomplete="off" id="password" name="password" type="password" class="text-2xl 
+                                peer placeholder-transparent h-10 w-full border-b-2 border-gray-300
                                      text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password"
                                     v-model="AuthAdmin.password" />
-                                <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base
-                                     peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5
-                                      peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                <label for="password" class="absolute left-0 -top-3.5 text-black
+         text-xl sm:text-base md:text-3xl lg:text-xl
+         transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-gray-400
+         peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                             </div>
                             <div class="relative">
-                                <button class="bg-cyan-500 text-white rounded-md px-2 py-1" @click="connexionDashboard">
+                                <button
+                                    class="bg-cyan-500 text-white rounded-md px-4 py-2
+         text-xl sm:text-base md:text-3xl lg:text-lg
+         hover:bg-cyan-600 transition duration-300 cursor-pointer transform translate-y-6 lg:translate-y-4 md:translate-y-8"
+                                    @click="connexionDashboard">
                                     connexion
                                 </button>
                             </div>
@@ -68,7 +76,7 @@ const connexionDashboard = async () => {
         }
 
     } catch (error) {
-        message.value = error.response?.data?.message || 'Erreur lors de la connexion.'; 
+        message.value = error.response?.data?.message || 'Erreur lors de la connexion.';
     }
 };
 </script>
